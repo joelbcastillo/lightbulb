7f63b3663970e024006462deedf09a09ca9e2b6d
SamJWeissman <SamJWeissman@gmail.com>
2015-03-05 23:51:31 -0500

    Added Basic Templating
    Based on Discover Meteor Book

---
+++ client/templates/posts/post_item.js
@@ -0,0 +1,10 @@
+/**
+ * Created by joel on 3/5/15.
+ */
+Template.postItem.helpers({
+    domain: function() {
+        var a = document.createElement('a');
+        a.href = this.url;
+        return a.hostname;
+    }
+});
