cba60ef228e51b96d2499666da9a906e5c4a166f
SamJWeissman <SamJWeissman@gmail.com>
2015-03-17 21:10:12 -0400

    simple user account page ui and functionality

--- client/templates/projects/project_edit.js
+++ client/templates/projects/project_edit.js
@@ -2,29 +2,19 @@ Template.projectEdit.events({
     'submit form': function(event) {
         event.preventDefault();
 
-        var currentProjectId = this._id;
-
-        var projectProperties = {
-            name: $(event.target).find('[name=name]').val(),
-            description: $(event.target).find('[name=description]').val()
-        };
-
-        Projects.update(currentProjectId, {$set: projectProperties}, function (error){
-            if (error) {
-                alert(error.reason);
-            } else {
-                Router.go('projectPage', {_id: currentProjectId});
-            }
-        });
-    },
-
-    'click .delete': function(event) {
-        event.preventDefault();
-
-        if (confim("Delete this project?")) {
-            var currentProjectId = this.id;
-            Projects.remove(currentProjectId);
-            Router.go('projectsList');
-        }
+        //var currentProjectId = this._id;
+        //
+        //var projectProperties = {
+        //    name: $(event.target).find('[name=name]').val(),
+        //    description: $(event.target).find('[name=description]').val()
+        //};
+        //
+        //Projects.update(currentProjectId, {$set: projectProperties}, function (error){
+        //    if (error) {
+        //        alert(error.reason);
+        //    } else {
+        //        Router.go('projectPage', {_id: currentProjectId});
+        //    }
+        //});
     }
 });
