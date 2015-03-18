0bd9a9b74151073dd3c72c13755d302c50786b16
Javlon Usmanov <javlon@nyu.edu>
2015-03-18 12:28:05 -0400

    home screen forms implemented

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -1,3 +1,23 @@
 Accounts.ui.config({
-    passwordSignupFields: 'USERNAME_ONLY'
+    passwordSignupFields: 'USERNAME_ONLY',
+    requestPermissions: {},
+    extraSignupFields: [{
+        fieldName: 'first-name',
+        fieldLabel: 'First name',
+        inputType: 'text',
+        visible: true,
+        saveToProfile: true
+    }, {
+        fieldName: 'last-name',
+        fieldLabel: 'Last name',
+        inputType: 'text',
+        visible: true,
+        saveToProfile: true
+    }, {
+        fieldName: 'terms',
+        fieldLabel: 'I accept the terms and conditions',
+        inputType: 'checkbox',
+        visible: true,
+        saveToProfile: false
+    }]
 });
--- client/templates/signup.js
+++ client/templates/signup.js
@@ -1,3 +1,24 @@
-Template.home.events({
+Template.signup.events({
+    'submit form' : function(event) {
+        event.preventDefault();
 
-})
+        username = $(event.target).find('[name=username]').val(),
+            firstName = $(event.target).find('[name=firstName]').val(),
+            lastName =  $(event.target).find('[name=lastName]').val(),
+            email= $(event.target).find('[name=email]').val(),
+            password =$(event.target).find('[name=password]').val()
+        
+        Accounts.createUser({
+            username: username,
+            email : email,
+            password : password,
+            profile  : {
+                firstname: firstName,
+                lastname: lastName
+            }
+
+        });
+        console.log('javlon rocks');
+    }
+
+});
