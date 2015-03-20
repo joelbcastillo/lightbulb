b4f25afdeb6ca1877b42dda4a457656e03a01615
Sam <samjweissman@gmail.com>
2015-03-19 22:46:43 -0400

    Fixed chat

--- server/publications.js
+++ server/publications.js
@@ -2,6 +2,10 @@ Meteor.publish('projects', function() {
    return Projects.find();
 });
 
-Meteor.publish('messages', function() {
-    return Messages.find();
+Meteor.publish('messages', function(options) {
+    check(options, {
+        sort: Object,
+        limit: Number
+    });
+    return Messages.find({}, options);
 });
