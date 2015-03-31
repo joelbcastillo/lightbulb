d31c3a791f6faa64db1e819002eb13384dd3df34
SamJWeissman <SamJWeissman@gmail.com>
2015-03-31 17:21:59 -0400

    no counts

--- client/templates/account/account.js
+++ client/templates/account/account.js
@@ -12,11 +12,11 @@ $('#teams a').click(function (e) {
     $(this).tab('show')
 });
 
-Template.account.helpers ({
-    selfProjectCount: function() {
-        return Projects.find({userId: Meteor.userId()}).count();
-    },
-    otherProjectCount: function () {
-        return Projects.find({userId: {$ne: Meteor.userId()}}).count();
-    }
-});
+//Template.account.helpers ({
+//    selfProjectCount: function() {
+//        return Projects.find({userId: Meteor.userId()}).count();
+//    },
+//    otherProjectCount: function () {
+//        //return Projects.find({userId: {$ne: Meteor.userId()}}).count();
+//    }
+//});
