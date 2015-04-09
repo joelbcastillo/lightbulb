a86df0034397589069c41e0418004f257ec2836b
Javlon Usmanov <javlon@nyu.edu>
2015-04-09 16:45:10 -0400

    user profile modified - additonal user attributes added

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -1,3 +1,44 @@
 Accounts.ui.config({
-    passwordSignupFields: 'USERNAME_ONLY'
+    passwordSignupFields: 'USERNAME_ONLY',
+    requestPermissions: {},
+    extraSignupFields: [{
+        fieldName: 'firstName',
+        fieldLabel: 'First name',
+        inputType: 'text',
+        visible: true,
+        saveToProfile: true
+    }, {
+        fieldName: 'lastName',
+        fieldLabel: 'Last name',
+        inputType: 'text',
+        visible: true,
+        saveToProfile: true
+    }, {
+        fieldName: 'email',
+        fieldLabel: 'Email',
+        inputType: 'text',
+        verified: true,
+        visible: true,
+        saveToProfile: true
+    }, {
+        fieldName: 'location',
+        fieldLabel: 'City, State',
+        inputType: 'text',
+        verified: true,
+        visible: true,
+        saveToProfile: true
+    }]
+
 });
+
+accountsUIBootstrap3.setCustomSignupOptions = function() {
+    //if we want to add refer codes, stuff that user doesnt have to know.
+    return {
+        referrerId: Session.get('referrerId') // Or whatever
+    }
+};
+
+accountsUIBootstrap3.logoutCallback = function(error) {
+    if(error) console.log("Error:" + error);
+    Router.go('home');
+};
--- server/accounts.js
+++ server/accounts.js
@@ -1,4 +1,5 @@
 Accounts.onCreateUser(function(options, user) {
     user.pendingProjects = [];
+    user.profile = options.profile ? options.profile : {};
     return user;
 });
