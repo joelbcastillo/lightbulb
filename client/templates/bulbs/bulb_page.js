a569e7830744303350a6a6259a6b1206f0ca9c01
Sam <SamJWeissman>
2015-04-18 10:59:47 -0400

    Added archiving of bulbs

--- client/templates/bulbs/bulb_page.js
+++ client/templates/bulbs/bulb_page.js
@@ -2,5 +2,16 @@ Template.bulbPage.helpers({
     // limit what is sent
     project : function() {
         return Projects.findOne({_id: this.projectId});//, {name: 1, description: 1});
+    },
+    archived: function() {
+        return this.isArchived;
     }
 });
+
+Template.bulbPage.events({
+    'click .archive': function(e) {
+        e.preventDefault();
+
+        Meteor.call('toggleArchived', this._id);
+    }
+})
