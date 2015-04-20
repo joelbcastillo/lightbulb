c48d401656c48ffb6932f4beb665d1797d8e69e8
SamJWeissman <SamJWeissman@gmail.com>
2015-04-20 15:14:04 -0400

    Made project edit modal functional

--- client/templates/projects/project_edit.js
+++ client/templates/projects/project_edit.js
@@ -1,20 +1,43 @@
-Template.projectEdit.events({
-    'submit form': function(event) {
-        event.preventDefault();
+Template.projectEdit.created = function () {
+    Session.set ('projectSubmitErrors', {});
+};
 
-        //var currentProjectId = this._id;
-        //
-        //var projectProperties = {
-        //    name: $(event.target).find('[name=name]').val(),
-        //    description: $(event.target).find('[name=description]').val()
-        //};
-        //
-        //Projects.update(currentProjectId, {$set: projectProperties}, function (error){
-        //    if (error) {
-        //        alert(error.reason);
-        //    } else {
-        //        Router.go('projectPage', {_id: currentProjectId});
-        //    }
-        //});
+Template.projectEdit.helpers({
+    errorMessage: function (field) {
+        return Session.get('projectSubmitErrors')[field];
+    },
+    errorClass: function (field) {
+        return !!Session.get('projectSubmitErrors')[field] ? 'has-error' : '';
+    }
+});
+
+Template.projectEdit.events({
+    'submit form': function(e, template) {
+        e.preventDefault();
+
+        var project = {
+            _id: template.data._id,
+            description: $(e.target).find('[name=description]').val(),
+            name: $(e.target).find('[name=name]').val()
+        };
+
+        var errors = validateProject(project);
+        if (errors.name || errors.url)
+            return Session.set('projectSubmitErrors', errors);
+
+        Meteor.call('projectEdit', project, function(error, result) {
+
+            if ( error )
+                return throwError(error.reason);
+
+            if (result.projectExists) {
+                throwError('This name has already been used');
+            }
+
+            $('#editProjectModal').modal('hide');
+            $('form[name="editProject"]').find('input[name="name"]').val('');
+            $('form[name="editProject"]').find('input[name="description"]').val('');
+            Router.go('projectPage', { _id: template.data._id});
+        });
     }
 });
