493074867e9329063680c7771532db6d82c08907
Sam <SamJWeissman>
2015-04-20 22:07:29 -0400

    Lots of aesthetic changes... still need a lot more

--- client/templates/bulbs/bulb_item.js
+++ client/templates/bulbs/bulb_item.js
@@ -1,8 +1,7 @@
 Template.bulbItem.helpers({
     luminance_css: function() {
         var opacity = (this.sparksCount > 0) ? this.curr_lum / this.max_lum : 0.5;
-        console.log('background: rgba(255,255,0,' + opacity + ')');
-        return 'background: rgba(255,255,0,' + opacity + ')';
+        return 'opacity: ' + opacity;
     },
     luminance_value: function() {
         var opacity = (this.sparksCount > 0) ? this.curr_lum / this.max_lum : 0.5;
--- client/templates/bulbs/bulb_page.js
+++ client/templates/bulbs/bulb_page.js
@@ -5,6 +5,14 @@ Template.bulbPage.helpers({
     },
     archived: function() {
         return this.isArchived;
+    },
+    luminance_css: function() {
+        var opacity = (this.sparksCount > 0) ? this.curr_lum / this.max_lum : 0.5;
+        return 'opacity: ' + opacity;
+    },
+    luminance_value: function() {
+        var opacity = (this.sparksCount > 0) ? this.curr_lum / this.max_lum : 0.5;
+        return Math.round(opacity * 100);
     }
 });
 
