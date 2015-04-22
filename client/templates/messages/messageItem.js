b6094ca4e92638cd1f27faac055897cb5f54ca21
Panagis Alisandratos <p9alisand@gmail.com>
2015-04-22 17:44:13 -0400

    header symbol and emailHandler.js added

--- client/templates/messages/messageItem.js
+++ client/templates/messages/messageItem.js
@@ -1,3 +1,7 @@
+/**
+ * Created by Sam on 4/20/2015.
+ */
+
 Template.messageItem.helpers({
     prettyDate: function() {
         return this.createdDate.toLocaleTimeString() + ", " + this.createdDate.toDateString();
